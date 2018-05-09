9b80434561564e08afc1c4aa43a7b300e1e33319
dan <daniloplay2009@hotmail.com>
2018-05-09 08:41:03 -0300

    app.module.ts alt0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { VisHojeComponent } from './vis-hoje/vis-hoje.component';
+import { CriarTarefaComponent } from './criar-tarefa/criar-tarefa.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisHojeComponent,
+    CriarTarefaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    routing
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
