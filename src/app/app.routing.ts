c905205296cabbc8a302e6cd9e17426ee950952d
dan <daniloplay2009@hotmail.com>
2018-05-15 10:21:19 -0300

    app-routing-ts 0.0.4

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { VisHojeComponent } from './vis-hoje/vis-hoje.component';
 import { CriarTarefaComponent } from './criar-tarefa/criar-tarefa.component';
 import { TabelaAgendaComponent } from './tabela-agenda/tabela-agenda.component';
+import { VisTarefaComponent } from './vis-tarefa/vis-tarefa.component';
 
 import { ModuleWithProviders } from '@angular/core';
 
@@ -9,7 +10,8 @@ const APP_ROUTES: Routes = [
        
     {path: 'criartarefa', component: CriarTarefaComponent},
     { path: '', component: VisHojeComponent  },
-    { path: 'tabela-agenda', component: TabelaAgendaComponent}    
+    { path: 'tabela-agenda', component: TabelaAgendaComponent},   
+    { path: 'vis-tarefa', component: VisTarefaComponent}
 
 ] ;
 
